4d0d377fbda7aed5291f0e7e21f498e9db6d87a5
Benjamin Doherty <bjd@bangpound.org>
2016-04-24 12:04:09 -0500

    add var dumper and enable its service provider in dev.

--- config/dev.php
+++ config/dev.php
@@ -1,6 +1,7 @@
 <?php
 
 use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\VarDumperServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
 
 // include the prod configuration
@@ -12,6 +13,8 @@ $app['debug'] = true;
 $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
 ));
+
+$app->register(new VarDumperServiceProvider());
 
 $app->register(new WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
