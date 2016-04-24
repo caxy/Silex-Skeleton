d589445ac5a50e08c5a6c656296f7221e7312a5d
Benjamin Doherty <bjd@bangpound.org>
2016-04-24 12:11:19 -0500

    silex controller resolver injects Application into controller functions.

--- src/controllers.php
+++ src/controllers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -8,7 +9,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
+$app->get('/', function (Application $app) {
     return $app['twig']->render('index.html.twig', array());
 })
 ->bind('homepage')
